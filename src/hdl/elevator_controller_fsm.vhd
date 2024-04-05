dc302ca61ddcc4c708c95cb4a8b74afe051083c9
sam-nissen <142038035+sam-nissen@users.noreply.github.com>
2024-04-04 21:14:44 -0600

    first attempt

--- src/hdl/elevator_controller_fsm.vhd
+++ src/hdl/elevator_controller_fsm.vhd
@@ -11,8 +11,8 @@
 --| ---------------------------------------------------------------------------
 --|
 --| FILENAME      : MooreElevatorController.vhd
---| AUTHOR(S)     : Capt Phillip Warner, Capt Dan Johnson, Capt Brian Yarbrough, ***YourName***
---| CREATED       : 03/2018 Last Modified on 06/24/2020
+--| AUTHOR(S)     : Capt Phillip Warner, Capt Dan Johnson, Capt Brian Yarbrough, C3C Sam Nissen
+--| CREATED       : 03/2018 Last Modified on 04/03/2024
 --| DESCRIPTION   : This file implements the ICE5 Basic elevator controller (Moore Machine)
 --|
 --|  The system is specified as follows:
@@ -96,20 +96,21 @@ begin
 
 	-- CONCURRENT STATEMENTS ------------------------------------------------------------------------------
 	-- Next State Logic
-    f_Q_next <= <state> when (<condition>) else -- going up
-                ...
-                ...
-                ... -- going down
-                ...
-                ... else
-                ...; -- default case
+    f_Q_next <= s_floor2 when (f_Q = s_floor1 AND i_up_down = '1') else -- going up
+                s_floor3 when (f_Q = s_floor2 AND i_up_down = '1') else
+                s_floor4 when (f_Q = s_floor3 AND i_up_down = '1') else
+                s_floor3 when (f_Q = s_floor4 AND i_up_down = '0') else -- going down
+                s_floor2 when (f_Q = s_floor3 AND i_up_down = '0') else
+                s_floor1 when (f_Q = s_floor2 AND i_up_down = '0') else
+                s_floor2; -- default case
   
 	-- Output logic
     with f_Q select
-        o_floor <= <value> when s_floor1,
-                ...
-                ...
-                <value> when others; -- default is floor 2
+        o_floor <= "0001" when s_floor1,
+                   "0010" when s_floor2,
+                   "0011" when s_floor3,
+                   "0100" when s_floor4,
+                   "0010" when others; -- default is floor 2
 	
 	-------------------------------------------------------------------------------------------------------
 	
@@ -118,11 +119,20 @@ begin
 	register_proc : process (i_clk)
     begin
          -- synchronous reset
-        
-        -- if elevator is enabled, advance floors
-        -- if not enabled, stay at current floor
-    
-	end process register_proc;	
+        if (rising_edge(i_clk)) then
+            if i_reset = '1' then
+                f_Q <= s_floor2;
+            end if;
+                
+            if i_stop = '0' then -- if elevator is enabled, advance floors
+                f_Q <= f_Q_next;
+            elsif i_stop = '1' then -- if not enabled, stay at current floor
+                f_Q <= f_Q;   
+            end if;
+            
+        end if;
+            
+	end process register_proc;
 	
 	-------------------------------------------------------------------------------------------------------
 	
--- src/hdl/elevator_controller_fsm_tb.vhd
+++ src/hdl/elevator_controller_fsm_tb.vhd
@@ -11,8 +11,8 @@
 --| ---------------------------------------------------------------------------
 --|
 --| FILENAME      : MooreElevatorController_tb.vhd (TEST BENCH)
---| AUTHOR(S)     : Capt Phillip Warner, Capt Dan Johnson, **Your Name Here**
---| CREATED       : 03/2017 Last modified on 06/24/2020
+--| AUTHOR(S)     : Capt Phillip Warner, Capt Dan Johnson, C3C Sam Nissen
+--| CREATED       : 03/2017 Last modified on 04/03/2024
 --| DESCRIPTION   : This file tests the Moore elevator controller module
 --|
 --| DOCUMENTATION : None
@@ -103,19 +103,35 @@ begin
         w_reset <= '1';  wait for k_clk_period;
             assert w_floor = "0010" report "bad reset" severity failure; 
         -- clear reset
-		
+		w_reset <= '0';
 		-- active UP signal
-		w_up_down <= '1'; 
-		
+		w_up_down <= '1';
 		-- stay on each o_floor for 2 cycles and then move up to the next o_floor
         w_stop <= '1';  wait for k_clk_period * 2;
             assert w_floor = "0010" report "bad wait on floor2" severity failure;
         w_stop <= '0';  wait for k_clk_period;
             assert w_floor = "0011" report "bad up from floor2" severity failure;
 		-- rest of cases
-        
+        w_stop <= '1'; wait for k_clk_period * 2;
+            assert w_floor = "0011" report "bad wait on floor3" severity failure;
+        w_stop <= '0'; wait for k_clk_period * 3;
+            assert w_floor = "0100" report "bad up from floor3" severity failure;
         -- go back DOWN
-          
+        w_up_down <= '0';
+        w_stop <= '1'; wait for k_clk_period * 2;
+            assert w_floor = "0100" report "bad wait on floor4" severity failure;
+        w_stop <= '0'; wait for k_clk_period;
+            assert w_floor = "0011" report "bad down from floor4" severity failure;
+        w_stop <= '1'; wait for k_clk_period * 2;
+            assert w_floor = "0011" report "bad wait on floor3" severity failure;
+        w_stop <= '0'; wait for k_clk_period;
+            assert w_floor = "0010" report "bad down from floor3" severity failure;
+        w_stop <= '1'; wait for k_clk_period * 2;
+            assert w_floor = "0010" report "bad wait on floor2" severity failure;
+        w_stop <= '0'; wait for k_clk_period;
+            assert w_floor = "0001" report "bad down from floor2" severity failure;
+        w_stop <= '1'; wait for k_clk_period * 3;
+            assert w_floor = "0001" report "bad wait on floor1" severity failure;
 		  	
 		wait; -- wait forever
 	end process;	
